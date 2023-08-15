a3249fc665b166cf78e5ba4cffbb701f5cfb77fd
Halex-64 <senai@DSN1191137658.edu119.local>
2023-08-15 11:09:26 -0300

    Organizando arquivos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
-import { CorpoComponent } from './corpo/corpo.component';
+import { CorpoComponent } from './componentes/corpo/corpo.component';
+import { AdicionarComponent } from './componentes/adicionar/adicionar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CabecalhoComponent,
-    CorpoComponent
+    CorpoComponent,
+    AdicionarComponent
   ],
   imports: [
     BrowserModule,
