b9a5cdf6a5c6dbffa37111a92ef7f93086b6ec20
Halex-64 <halexgoncalves6@gmail.com>
2023-09-11 09:26:15 -0300

    adicionando a interface

--- src/app/componentes/nota/nota.component.ts
+++ src/app/componentes/nota/nota.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Nota } from '../nota';
 
 @Component({
   selector: 'app-nota',
@@ -7,7 +8,8 @@ import { Component, Input } from '@angular/core';
 })
 export class NotaComponent {
 
-  @Input() nota = {
+  @Input() nota:Nota = {
+      id: 0,
       titulo: "Titulo",
       conteudo: "Conteudo Teste",
       tipo:"tipo1",
